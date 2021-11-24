bc3701f5e07c48f092f38b5bcf7f50bf86282863
subedia3 <subediashok900@gmail.com>
2021-11-23 19:32:13 -0500

    Added settings listview

--- app/src/main/java/com/example/version3_355app/ui/settings/Settings.java
+++ app/src/main/java/com/example/version3_355app/ui/settings/Settings.java
@@ -1,4 +1,37 @@
 package com.example.version3_355app.ui.settings;
 
-public class Settings {
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.version3_355app.R;
+
+import java.util.ArrayList;
+
+public class Settings extends AppCompatActivity{
+    ArrayList<String> settingsList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_settings);
+
+        ListView listView = (ListView) findViewById(R.id.listview); // list in your XML
+
+        settingsList.add("Account");
+        settingsList.add("Notifications");
+        settingsList.add("Theme");
+
+    }
 }
+
