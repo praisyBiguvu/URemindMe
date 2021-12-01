007a8b8d016a5401974b38f4a9f0ced8194cc070
subedia3 <subediashok900@gmail.com>
2021-11-30 21:45:56 -0500

    Implemented logic for dark, light, and system themes

--- app/src/main/java/com/example/version3_355app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/version3_355app/ui/settings/SettingsFragment.java
@@ -1,32 +1,47 @@
 package com.example.version3_355app.ui.settings;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
-
 import com.example.version3_355app.R;
-import com.example.version3_355app.databinding.FragmentCalendarBinding;
-import androidx.preference.PreferenceFragmentCompat;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.prefrerences, rootKey);
+
+        PreferenceScreen screen = getPreferenceScreen();
+        final ListPreference listPreference = (ListPreference) findPreference("list_prefrence_key");
+        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                listPreference.setValue(newValue.toString());
+                String theme = String.valueOf(listPreference.getEntry());
+                if(theme.equals("Light")){
+                    System.out.println("light");
+
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                if(theme.equals("Dark")){
+                    System.out.println("dark");
+
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                if(theme.equals("System")){
+                    System.out.println("sytem");
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                }
+
+                return true;
+            }
+        });
     }
+
+
 }
-
-
-
