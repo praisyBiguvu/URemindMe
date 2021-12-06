36a244cab8b0d3025bffe7872735ceffcf47961b
Praisy <biguvup@vcu.edu>
2021-12-06 11:31:12 -0500

    Data Model for ToDo --support files

--- app/src/main/java/com/example/version3_355app/ui/to_do/ToDoFormFragment.java
+++ app/src/main/java/com/example/version3_355app/ui/to_do/ToDoFormFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,9 +14,13 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.version3_355app.R;
 import com.example.version3_355app.databinding.FragmentFormTodoBinding;
 
+
 public class ToDoFormFragment extends Fragment {
 
     private FragmentFormTodoBinding binding;
+    Button buttonSecond, backButton;
+    EditText et_assignment, et_date, et_time, et_class;
+
 
     @Override
     public View onCreateView(
@@ -29,10 +35,18 @@ public class ToDoFormFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        buttonSecond= getView().findViewById(R.id.button_second);
+        backButton= getView().findViewById(R.id.backButton);
+        et_assignment= getView().findViewById(R.id.et_assignment);
+        et_date= getView().findViewById(R.id.et_date);
+        et_time = getView().findViewById(R.id.et_time);
+//        et_class= getView().findViewById(R.id.et_class);
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                TodoModel to_do_form= new TodoModel(et_assignment.getText().toString(), et_date., et_time.getText().toString(), "TBD");
                 NavHostFragment.findNavController(ToDoFormFragment.this)
                         .navigate(R.id.nav_action_form_to_cal);
             }
--- app/src/main/java/com/example/version3_355app/ui/to_do/TodoModel.java
+++ app/src/main/java/com/example/version3_355app/ui/to_do/TodoModel.java
@@ -1,2 +1,62 @@
-package com.example.version3_355app.ui.to_do;public class TodoModel {
+package com.example.version3_355app.ui.to_do;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class TodoModel {
+    private String Assignment;
+    private Date date;
+    private Time time;
+    private String course;
+
+    public TodoModel(String Assignment, Date date, Time time, String course){
+        this.Assignment= Assignment;
+        this.date=date;
+        this.time=time;
+        this.course=course;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoModel{" +
+                "Assignment='" + Assignment + '\'' +
+                ", date=" + date +
+                ", time=" + time +
+                ", course='" + course + '\'' +
+                '}';
+    }
+
+    public String getAssignment() {
+        return Assignment;
+    }
+
+    public void setAssignment(String assignment) {
+        Assignment = assignment;
+    }
+
+    public Date getDate() {
+        
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
+
