9b8643264f70f3d84e1154e03d169fec26d4cee1
subedia3 <subediashok900@gmail.com>
2021-12-13 17:20:37 -0500

    Linked splash activity to layout resources

--- app/src/main/java/com/example/version3_355app/SplashActivity.java
+++ app/src/main/java/com/example/version3_355app/SplashActivity.java
@@ -1,4 +1,16 @@
 package com.example.version3_355app;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
